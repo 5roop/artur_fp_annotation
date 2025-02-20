4d7cee299285a5e22296cbfa4995365b69183a0c
Peter Rupnik <rupnikpeter@gmail.com>
2025-02-20 13:24:09 +0100

    Finish writing the utterance extractor

---
+++ 1_read_utterances.py
@@ -0,0 +1,33 @@
+"""
+Goal: Parse the TEIs and generate a dataset of all utterances with some metadata
+"""
+
+from pathlib import Path
+import polars as pl
+
+pl.Config.set_tbl_cols(-1)
+# pl.Config.set_tbl_rows(-1)
+pl.Config.set_tbl_width_chars(700)
+pl.Config.set_fmt_str_lengths(100)
+from utils import extract_utterances, find_audio
+
+TEIs = list(Path("Gos.TEI/").glob("Artur-*/*.xml"))
+results = []
+for p in TEIs:
+    data = extract_utterances(p)
+    results.extend(data)
+    2 + 2
+df = pl.DataFrame(results).with_columns(duration=pl.col("end_s") - pl.col("start_s"))
+
+print(df)
+print("Adding filenames:")
+
+files = pl.DataFrame(df["filename"].unique()).with_columns(
+    audio_file=pl.col("filename").map_elements(find_audio, return_dtype=pl.String)
+)
+df = df.join(files, on="filename", how="left")
+print("Filtering out instances with no words:")
+df = df.filter(pl.col("w_count") > 0)
+print(df)
+2 + 2
+df.write_ndjson("1_utterances.jsonl")
---
+++ utils.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+
+
+def extract_utterances(p: str | Path):
+    from lxml import etree
+
+    doc = etree.fromstring(Path(p).read_bytes())
+    prefix = "{http://www.w3.org/XML/1998/namespace}"
+    whens = doc.findall(".//{*}when")
+    timeline = {
+        when.attrib[f"{prefix}id"]: float(when.attrib.get("interval", "0.0"))
+        for when in whens
+    }
+    us = doc.findall(".//{*}u")
+
+    def format_u(u):
+        ident = u.attrib.get(f"{prefix}id", None)
+        who = u.attrib.get("who", None)
+        try:
+            who = who.replace("#", "")
+        except:
+            pass
+        start = u.attrib.get("start", None)
+        try:
+            start = start.replace("#", "")
+        except:
+            pass
+        end = u.attrib.get("end", None)
+        try:
+            end = end.replace("#", "")
+        except:
+            pass
+        start_s = timeline.get(start, None)
+        end_s = timeline.get(end, None)
+        w_count = len(u.findall(".//{*}w"))
+        return dict(
+            ident=ident,
+            who=who,
+            start=start,
+            end=end,
+            start_s=start_s,
+            end_s=end_s,
+            w_count=w_count,
+            path=str(p),
+            filename=Path(p).name,
+        )
+
+    return [format_u(u) for u in us]
+
+
+def find_audio(p: str | Path):
+    p = Path(p).with_suffix("").with_suffix("").name
+    hits = list(Path(".").glob(f"**/{p}*.wav"))
+    if len(hits) == 1:
+        return str(hits[0])
+    elif len(hits) == 0:
+        raise AttributeError("No hits found!")
+    else:
+        raise AttributeError("Multiple hits found!")
